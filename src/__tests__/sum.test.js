a209fbc2a59c35b3e84b471a1c2c5e3127d217ce
Luke Houge <lukehouge@gmail.com>
2022-02-08 12:15:24 -0600

    breaking test

--- src/__tests__/sum.test.js
+++ src/__tests__/sum.test.js
@@ -7,5 +7,5 @@ test('adds 2 + 5 to equal 7', () => {
     expect(sum(2, 5)).toBe(7)
 })
 test('adds 1.2 + 9.5 to equal 10.7', () => {
-    expect(sum(1.2, 9.5)).toBe(10.7)
+    expect(sum(1.2, 9.5)).toBe(10.8)
 })

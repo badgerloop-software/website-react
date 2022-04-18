6298317234002b08dd70fa83eb8f26121b517738
jedschlo14 <jedschloesser@gmail.com>
2022-04-18 17:37:04 -0500

    Modified npm tests

--- src/pages/Donate.js
+++
@@ -1,9 +0,0 @@
-import Navbar from '../components/Navbar'
-
-export default function Donate() {
-    return (
-        <div className="absolute h-screen w-screen bg-gray-300 overflow-auto text-gray-700">
-            <Navbar />
-        </div>
-    )
-}
